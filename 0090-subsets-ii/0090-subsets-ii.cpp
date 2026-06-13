9f0013cb628ef6ca7ec27e0a8ed68436eb8d4bcc
Ajeet singh <149367783+ajeetuncadmay@users.noreply.github.com>
2026-06-13 09:15:52 +0530

    Time: 0 ms (100%), Space: 10.7 MB (43.74%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+
+    void backtrack(vector<int>& nums, int start, vector<int>& temp) {
+        result.push_back(temp);
+
+        for (int i = start; i < nums.size(); i++) {
+
+            // Skip duplicates in same recursion level
+            if (i > start && nums[i] == nums[i - 1])
+                continue;
+
+            temp.push_back(nums[i]);
+            backtrack(nums, i + 1, temp);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> temp;
+
+        backtrack(nums, 0, temp);
+        return result;
+    }
+};
