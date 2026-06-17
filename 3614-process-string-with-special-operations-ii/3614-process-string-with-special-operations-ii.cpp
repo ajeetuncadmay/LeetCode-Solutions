ac5d31dc29bd604a078300da15b9d92849cec34a
Ajeet singh <149367783+ajeetuncadmay@users.noreply.github.com>
2026-06-17 06:24:21 +0530

    Time: 8 ms (98.85%), Space: 24.1 MB (51.72%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        long long len = 0;
+
+        // Calculate final length
+        for (char c : s) {
+            if (c == '*') {
+                if (len > 0) len--;
+            } 
+            else if (c == '#') {
+                len *= 2;
+            } 
+            else if (c == '%') {
+                // length unchanged
+            } 
+            else {
+                len++;
+            }
+        }
+
+        if (k >= len) return '.';
+
+        // Reverse process
+        for (int i = s.size() - 1; i >= 0; i--) {
+            char c = s[i];
+
+            if (c == '*') {
+                len++;
+            }
+            else if (c == '#') {
+                len /= 2;
+                if (k >= len) k -= len;
+            }
+            else if (c == '%') {
+                k = len - 1 - k;
+            }
+            else { // letter
+                len--;
+                if (k == len) return c;
+            }
+        }
+
+        return '.';
+    }
+};
